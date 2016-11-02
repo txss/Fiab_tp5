89f0af3638e92c3f2d8d896096980350440eb139
Hayanno <nicolas@leotier.fr>
2016-11-02 18:59:58 +0100

    question 5

--- src/date/MyDateException.java
+++ src/date/MyDateException.java
@@ -1,6 +1,11 @@
 package date;
 
 public class MyDateException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public MyDateException(String string){ super(); }
 	
 	 
